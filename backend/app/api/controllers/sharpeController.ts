5bf7d7e72665a78f61b4a39d388fb4291ead4493
Ryan Sharkey <ryansharkey@Ryans-Mac-mini.lan>
2023-08-09 08:11:03 -0400

    stock check

--- backend/app/api/controllers/sharpeController.ts
+++ backend/app/api/controllers/sharpeController.ts
@@ -1,33 +1,43 @@
 import { GetSharpeRatioInput, calculateSharpeRatio, GetSharpeRatioOutput, StockData } from "../models/sharpe"
-import { Request, Response, NextFunction } from "express"
+import { Request, Response, } from "express"
+import * as tickerData from "../consts/validStock.json";
+
+
 
 
 export async function get_sharpe_ratio(req: Request, res: Response) {
     var sharpeRatioInput = new GetSharpeRatioInput();
     var portfolio: StockData[] = []
     var totalShares = 0;
+    let errorsent = false;
 
 
     Object.entries(req.query).forEach(([key, value]: [string, any]) => {
-        // add check if valid stock ticker
         if (key == "years") {
             sharpeRatioInput.years = parseInt(value);
         }
         else {
+            if (!(key in tickerData)) {
+                res.status(400).send({ error: `Ticker ${key} not found` });
+                errorsent = true;
+                return;
+            }
 
             totalShares += parseInt(value);
             var portfolioData = new StockData(key, parseInt(value));
             portfolio.push(portfolioData)
         }
     });
-
+    if (errorsent) {
+        return;
+    }
 
     sharpeRatioInput.portfolio = portfolio;
     sharpeRatioInput.totalShares = totalShares;
 
     var output = await calculateSharpeRatio(sharpeRatioInput)
 
-    res.send(output);
+    res.status(200).send(output);
 
 }
 
